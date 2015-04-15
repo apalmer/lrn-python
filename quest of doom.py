3689a3a546b23caac0e32f72d549349b4ea70877
ajanip <scubaten@hotmail.com>
2015-04-15 18:44:43 -0400

    began story
    beginnings of text based adventure

--- quest of doom.py
+++ quest of doom.py
@@ -1,1 +1,7 @@
 print(" yo bro do me a solid and get me a cake")
+print(" ifyou do it then i will tell deats")
+name=input("what is your name,sir?")
+print("hello " + name + " im back from the market")
+GOOD Job
+
+HERE Are the deats mandy stole jim's gym socks
